9639f628815d72c21324e010f956d92c941fa942
shtnkgm <shtnkgm@gmail.com>
2019-03-17 23:06:35 +0900

    Initial Commit

---
+++ UserNotificationsSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserNotificationsSample
+//
+//  Created by Shota Nakagami on 2019/03/17.
+//  Copyright © 2019 Shota Nakagami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
